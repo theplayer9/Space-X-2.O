e22ca04246ccfe97c51742fc893433403ba03e62
Om Sharma <omsharma.theplayer11@gmail.com>
2022-05-08 15:39:01 +0530

    Set the background image. But it's not responsive !

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,12 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
+
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Space X- 2.O</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
